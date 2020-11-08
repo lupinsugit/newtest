869eed139f3b29249db695d0277946fc2571328b
lupinsugit <74125728+lupinsugit@users.noreply.github.com>
2020-11-08 15:53:12 +0800

    Add files via upload

---
+++ gittest.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class gittest {
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+		System.out.ptintln("Hello Github!");
+		System.out.println("Goodbye Git!");
+	}
+
+}
